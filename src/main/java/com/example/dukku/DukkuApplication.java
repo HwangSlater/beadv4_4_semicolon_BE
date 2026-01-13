0d36a912d3f4cd6523fef738b47134853a430f6b
ooooo_ori <dhtp0112@gmail.com>
2026-01-13 15:53:19 +0900

    infra: 초기 백엔드 설정 및 Docker 실행 환경 구성

---
+++ src/main/java/com/example/dukku/DukkuApplication.java
@@ -0,0 +1,11 @@
+package com.example.dukku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukkuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DukkuApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/dukku/DukkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dukku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
