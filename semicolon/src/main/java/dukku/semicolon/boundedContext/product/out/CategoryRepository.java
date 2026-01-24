84577a955097295f5038e9cdd74d1fb7b5dbf456
HwangSlater <doorchan23@naver.com>
2026-01-24 13:20:27 +0900

    product 병합 충돌 문제로 제거

--- semicolon/src/main/java/dukku/semicolon/boundedContext/product/out/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package dukku.semicolon.boundedContext.product.out;
-
-import dukku.semicolon.boundedContext.product.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
